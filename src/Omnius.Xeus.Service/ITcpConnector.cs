aba78ddfa637477ad68708d1138951679bd0a3af
Lyrse <lyrise1984@gmail.com>
2020-01-06 01:19:14 +0900

    Fix TcpConnectorの実装を変更

--- src/Omnius.Xeus.Service/ITcpConnector.cs
+++ src/Omnius.Xeus.Service/ITcpConnector.cs
@@ -5,13 +5,7 @@ using Omnius.Xeus.Service.Primitives;
 
 namespace Omnius.Xeus.Service
 {
-    public interface ITcpConnectorFactory
-    {
-        ValueTask<ITcpConnector> Create(TcpConnectorOptions tcpConnectorOptions, IBufferPool<byte> bufferPool);
-    }
-
     public interface ITcpConnector : IPrimitiveConnector, IAsyncDisposable
     {
-        public static ITcpConnectorFactory Factory { get; }
     }
 }
