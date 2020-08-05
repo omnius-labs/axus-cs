7c0382c82d5ef790c0ffe0361273736a7b818f00
Lyrise <lyrise1984@gmail.com>
2020-08-05 23:11:10 +0900

    fix: gen-code.sh

--- eng/gen-code.sh
+++ eng/gen-code.sh
@@ -7,7 +7,7 @@ TOOL_PATH="$BIN_DIR/Omnius.Core.Serialization.RocketPack.DefinitionCompiler/Omni
 INCLUDE_1="$PWD/refs/core/fmt"
 INCLUDE_2="$PWD/fmt"
 
-"$TOOL_PATH" -f "$PWD/fmt/Omnius.Xeus.Service/Omnius.Xeus.Service.Models.rpd" -i "$INCLUDE_1" "$INCLUDE_2" -o "$PWD/src/Omnius.Xeus.Service/Models/_RocketPack/Messages.generated.cs"
-"$TOOL_PATH" -f "$PWD/fmt/Omnius.Xeus.Service.Impls/Omnius.Xeus.Service.Connectors.Internal.rpd" -i "$INCLUDE_1" "$INCLUDE_2" -o "$PWD/src/Omnius.Xeus.Service.Impls/Connectors/Internal/_RocketPack/Messages.generated.cs"
-"$TOOL_PATH" -f "$PWD/fmt/Omnius.Xeus.Service.Impls/Omnius.Xeus.Service.Storages.Internal.rpd" -i "$INCLUDE_1" "$INCLUDE_2" -o "$PWD/src/Omnius.Xeus.Service.Impls/Storages/Internal/_RocketPack/Messages.generated.cs"
-"$TOOL_PATH" -f "$PWD/fmt/Omnius.Xeus.Service.Impls/Omnius.Xeus.Service.Engines.Internal.rpd" -i "$INCLUDE_1" "$INCLUDE_2" -o "$PWD/src/Omnius.Xeus.Service.Impls/Engines/Internal/_RocketPack/Messages.generated.cs"
+"$TOOL_PATH" -s "$PWD/fmt/Omnius.Xeus.Service/Omnius.Xeus.Service.Models.rpd" -i "$INCLUDE_1" -i "$INCLUDE_2" -o "$PWD/src/Omnius.Xeus.Service/Models/_RocketPack/Messages.generated.cs"
+"$TOOL_PATH" -s "$PWD/fmt/Omnius.Xeus.Service.Impls/Omnius.Xeus.Service.Connectors.Internal.rpd" -i "$INCLUDE_1" -i "$INCLUDE_2" -o "$PWD/src/Omnius.Xeus.Service.Impls/Connectors/Internal/_RocketPack/Messages.generated.cs"
+"$TOOL_PATH" -s "$PWD/fmt/Omnius.Xeus.Service.Impls/Omnius.Xeus.Service.Storages.Internal.rpd" -i "$INCLUDE_1" -i "$INCLUDE_2" -o "$PWD/src/Omnius.Xeus.Service.Impls/Storages/Internal/_RocketPack/Messages.generated.cs"
+"$TOOL_PATH" -s "$PWD/fmt/Omnius.Xeus.Service.Impls/Omnius.Xeus.Service.Engines.Internal.rpd" -i "$INCLUDE_1" -i "$INCLUDE_2" -o "$PWD/src/Omnius.Xeus.Service.Impls/Engines/Internal/_RocketPack/Messages.generated.cs"
