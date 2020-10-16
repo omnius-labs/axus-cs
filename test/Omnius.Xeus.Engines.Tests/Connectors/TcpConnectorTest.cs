a8165c58762044f42a37af7dd3c5e622de173a9a
Lyrise <lyrise1984@gmail.com>
2020-10-16 09:55:28 +0900

    fix

--- test/Omnius.Xeus.Engines.Tests/Connectors/TcpConnectorTest.cs
+++ test/Omnius.Xeus.Engines.Tests/Connectors/TcpConnectorTest.cs
@@ -1,5 +1,13 @@
 using System.Threading.Tasks;
+using Omnius.Xeus.Engines.Models;
 using Xunit;
+using Moq;
+using Omnius.Core.Network.Proxies;
+using Omnius.Core.Network.Upnp;
+using System.Net;
+using System;
+using Omnius.Core.Network;
+using Omnius.Core;
 
 namespace Omnius.Xeus.Engines.Connectors
 {
@@ -8,6 +16,21 @@ namespace Omnius.Xeus.Engines.Connectors
         [Fact]
         public async Task ConnectAsyncSuccessTest()
         {
+            int Port = 55555;
+            var IpAddress = IPAddress.Parse("127.0.0.1");
+
+            var tcpConnectingOptions = new TcpConnectingOptions(true, null);
+            var tcpAcceptingOptions = new TcpAcceptingOptions(false, Array.Empty<OmniAddress>(), false);
+            var bandwidthOptions = new BandwidthOptions(1024, 1024);
+            var TcpConnectorOptions = new TcpConnectorOptions(tcpConnectingOptions, tcpAcceptingOptions, bandwidthOptions);
+
+            var socks5ProxyClient = new Mock<ISocks5ProxyClient>();
+            var socks5ProxyClientFactoryMock = new Mock<ISocks5ProxyClientFactory>();
+            var httpProxyClientFactoryMock = new Mock<IHttpProxyClientFactory>();
+            var upnpClientFactoryMock = new Mock<IUpnpClientFactory>();
+            var bytesPool = BytesPool.Shared;
+
+            var t = TcpConnector.Factory.CreateAsync(TcpConnectorOptions, socks5ProxyClientFactory, httpProxyClientFactory, upnpClientFactory, bytesPool);
         }
 
         [Fact]
