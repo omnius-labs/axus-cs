4470d2dacb76651f416aa384ad5180692397d433
Lyrise <lyrise1984@gmail.com>
2022-01-04 21:45:01 +0900

    fix: AxisMessage

--- src/Omnius.Axis.Intaractors/Implementations/AxisMessage.cs
+++ src/Omnius.Axis.Intaractors/Implementations/AxisMessage.cs
@@ -8,8 +8,6 @@ using Omnius.Core.Serialization;
 
 namespace Omnius.Axis.Intaractors;
 
-// FIXME CRCをつける
-// FIXME バージョン情報を付ける
 public static class AxisMessage
 {
     private static readonly string _schema = "axis";
@@ -18,27 +16,27 @@ public static class AxisMessage
 
     public static string NodeToString(NodeLocation message)
     {
-        return MessageToString<NodeLocation>(_nodePath, message);
+        return MessageToString<NodeLocation>(_nodePath, 1, message);
     }
 
     public static bool TryStringToNode(string text, [NotNullWhen(true)] out NodeLocation? message)
     {
         message = null;
-        return TryStringToMessage<NodeLocation>(_nodePath, text, out message);
+        return TryStringToMessage<NodeLocation>(_nodePath, 1, text, out message);
     }
 
     public static string SeedToString(Seed message)
     {
-        return MessageToString<Seed>(_seedPath, message);
+        return MessageToString<Seed>(_seedPath, 1, message);
     }
 
     public static bool TryStringToSeed(string text, [NotNullWhen(true)] out Seed? message)
     {
         message = null;
-        return TryStringToMessage<Seed>(_seedPath, text, out message);
+        return TryStringToMessage<Seed>(_seedPath, 1, text, out message);
     }
 
-    private static string MessageToString<T>(string path, T message)
+    private static string MessageToString<T>(string path, int version, T message)
         where T : IRocketMessage<T>
     {
         var bytesPool = BytesPool.Shared;
@@ -47,48 +45,38 @@ public static class AxisMessage
         message.Export(inBytesPipe.Writer, bytesPool);
 
         using var outBytesPipe = new BytesPipe(bytesPool);
-        if (!OmniMessageConverter.TryWrite(1, inBytesPipe.Reader.GetSequence(), outBytesPipe.Writer)) throw new Exception();
+        if (!OmniMessageConverter.TryWrite(inBytesPipe.Reader.GetSequence(), outBytesPipe.Writer)) throw new Exception();
 
-        return AddSchemaAndPath(path, OmniBase.Encode(outBytesPipe.Reader.GetSequence(), ConvertStringType.Base58)!);
+        return AddSchemaAndPath(path, version, OmniBase.Encode(outBytesPipe.Reader.GetSequence(), ConvertStringType.Base58)!);
     }
 
-    private static bool TryStringToMessage<T>(string path, string text, [NotNullWhen(true)] out T? message)
+    private static bool TryStringToMessage<T>(string path, int version, string text, [NotNullWhen(true)] out T? message)
         where T : IRocketMessage<T>
     {
-        if (!TryRemoveSchemaAndPath(text, path, out var value))
-        {
-            message = default!;
-            return false;
-        }
+        message = default!;
+
+        if (!TryRemoveSchemaAndPath(text, path, version, out var value)) return false;
 
         var bytesPool = BytesPool.Shared;
 
         using var inBytesPipe = new BytesPipe(bytesPool);
-        if (!OmniBase.TryDecode(value, inBytesPipe.Writer))
-        {
-            message = default!;
-            return false;
-        }
+        if (!OmniBase.TryDecode(value, inBytesPipe.Writer)) return false;
 
         using var outBytesPipe = new BytesPipe(bytesPool);
-        if (!OmniMessageConverter.TryRead(inBytesPipe.Reader.GetSequence(), out var version, outBytesPipe.Writer))
-        {
-            message = default!;
-            return false;
-        }
+        if (!OmniMessageConverter.TryRead(inBytesPipe.Reader.GetSequence(), outBytesPipe.Writer)) return false;
 
         message = IRocketMessage<T>.Import(outBytesPipe.Reader.GetSequence(), bytesPool);
         return true;
     }
 
-    private static string AddSchemaAndPath(string path, string value)
+    private static string AddSchemaAndPath(string path, int version, string value)
     {
-        return $"{_schema}:{path}/{value}";
+        return $"{_schema}:{path}/{version}/{value}";
     }
 
-    private static bool TryRemoveSchemaAndPath(string text, string path, [NotNullWhen(true)] out string? value)
+    private static bool TryRemoveSchemaAndPath(string text, string path, int version, [NotNullWhen(true)] out string? value)
     {
-        var targetPrefix = $"{_schema}:{path}/";
+        var targetPrefix = $"{_schema}:{path}/{version}/";
 
         value = null;
         if (!text.StartsWith(targetPrefix)) return false;
