11edc1d1637a0d5822ff80f42ac9adc878f9de41
Lyrise <lyrise1984@gmail.com>
2022-07-01 05:32:02 +0900

    update: lock files

--- eng/update-sln.sh
+++ eng/update-sln.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 dotnet new sln --force -n axis
-dotnet sln axis.sln add ./src/**/*.csproj
-dotnet sln axis.sln add ./test/**/*.csproj
 dotnet sln axis.sln add ./refs/core/src/**/*.csproj
 dotnet sln axis.sln add ./refs/core/test/**/*.csproj
+dotnet sln axis.sln add ./src/**/*.csproj
+dotnet sln axis.sln add ./test/**/*.csproj
