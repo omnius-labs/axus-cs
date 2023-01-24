1bed606a7535f8abfd7e5487999cf54f2641e906
Lyrise <lyrise1984@gmail.com>
2023-01-24 16:05:13 +0900

    fix

--- eng/gen-envfile.sh
+++ eng/gen-envfile.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 rm -f .env
 SCREEN=$(xrandr --listactivemonitors | awk -F " " '{ printf("%s", $4) }')
-echo "AVALONIA_SCREEN_SCALE_FACTORS='$SCREEN=$SCALE'" >> .env
+echo "AVALONIA_SCREEN_SCALE_FACTORS='$SCREEN=${SCREEN_SCALE:-1}'" >> .env
 echo "LANG=en_US.UTF-8" >> .env
