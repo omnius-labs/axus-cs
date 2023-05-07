54a4ee251b61f5540be45ced37570f4a9f5b4a74
Lyrise <lyrise1984@gmail.com>
2023-05-07 12:57:23 +0900

    fix

--- eng/update-sln.sh
+++ eng/update-sln.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 dotnet new sln --force -n axus
-dotnet sln axus.sln add ./refs/core/src/**/*.csproj
-dotnet sln axus.sln add ./refs/core/test/**/*.csproj
+dotnet sln axus.sln add ./refs/core-cs/src/**/*.csproj
+dotnet sln axus.sln add ./refs/core-cs/test/**/*.csproj
 dotnet sln axus.sln add ./src/**/*.csproj
 dotnet sln axus.sln add ./test/**/*.csproj
